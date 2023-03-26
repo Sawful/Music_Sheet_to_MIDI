382e236c6a41858a8a6ea240c5b59765a80b6b15
Colyn Co <colynco2@gmail.com>
2023-03-26 18:01:01 +0200

    Added create_contour() function and plot image

--- MSM_Main.py
+++ MSM_Main.py
@@ -1,17 +1,46 @@
-#Trouver un format d'image - #Definir un template d'image (et convertir si pas le bon template)
-#créer une nouvelle image
-#Cherche les notes --> Identifier les notes sur l'image + leur position (comparaison avec des fichiers de notes existantes)
-#Renvoyer une image (type MIDI)
-#
+# Trouver un format d'image - #Definir un template d'image (et convertir si pas le bon template)
+# créer une nouvelle image
+# Cherche les notes --> Identifier les notes sur l'image
+# leur position (comparaison avec des fichiers de notes existantes)
+# Renvoyer une image (type MIDI)
+
 from PIL import Image
+import cv2
+import numpy as np
 
 
-#source_folder = r'C:\Users\CO\AppData\Local\Temp'
-#for item in os.listdir(source_folder):
+def create_contour(img_path):
+
+    # reading image
+    image = cv2.imread(img_path)
+
+    # converting image into grayscale image
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # setting threshold of gray image
+    _, threshold = cv2.threshold(gray, 200, 255, cv2.THRESH_BINARY)
+
+    # using a findContours() function
+    contours_image, _ = cv2.findContours(
+        threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    return contours_image[1], image
 
 
-music_sheet_img = Image.open('musicsheet/bad_apple_musicsheet.png')
-music_sheet_img.show()
+noire_img_path = 'solfege/note/noire.png'
+noire_contour, noire_img = create_contour(noire_img_path)
 
+# cv2.approxPloyDP() function to approximate the shape
+approx = cv2.approxPolyDP(
+    noire_contour, 0.01 * cv2.arcLength(noire_contour, True), True)
 
-#C:\Users\CO\AppData\Local\Temp\tmp[random 8 caractères].PNG
+# using drawContours() function
+cv2.drawContours(noire_img, [noire_contour], 0, (0, 0, 255), 1)
+
+cv2.imshow('Contour', noire_img)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# music_sheet_img = Image.open('musicsheet/bad_apple_musicsheet.png')
+# music_sheet_img.show()
