8be8ed41298c6570966bd87fb50a2a36cb6aa06e
Colyn Co <colynco2@gmail.com>
2023-04-02 08:17:05 +0200

    WIP: Added approximate function

--- MSM_Main.py
+++ MSM_Main.py
@@ -27,17 +27,50 @@ def create_contour(img_path):
     return contours_image[1], image
 
 
+def approximate(reference_image_path, approximated_image_path):
+    # initialise a note counter
+    note_count = 0
+    # Initialise both reference image (with its contour) and approximated image
+    reference_image_contour, reference_image = create_contour(reference_image_path)
+    approximated_image = create_contour(approximated_image_path)[1]
+    # Get image dimensions for both images
+    reference_dimensions = reference_image.shape
+    image_dimensions = approximated_image.shape
+    # Approximate contour for the reference image
+    approx_ref = cv2.approxPolyDP(
+        reference_image_contour, 0.01 * cv2.arcLength(reference_image_contour, True), True)
+    # Scans whole image
+    for y in range(image_dimensions[0] - reference_dimensions[0]):
+        for x in range(image_dimensions[1] - reference_dimensions[1]):
+            # Define cropped image
+            cropped_image = approximated_image[x:(x+reference_dimensions[1]), y:(y+reference_dimensions[0])]
+            # Create a contour for each cropped image
+            gray = cv2.cvtColor(cropped_image, cv2.COLOR_BGR2GRAY)
+            _, threshold = cv2.threshold(gray, 200, 255, cv2.THRESH_BINARY)
+            contours_cropped, _ = cv2.findContours(
+                threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            approx = cv2.approxPolyDP(contours_cropped[0], 0.01 * cv2.arcLength(contours_cropped[0], True), True)
+            # Detect contour and compare if same as reference
+            if len(approx) == len(approx_ref):
+                print("noire")
+                note_count += 1
+    # How many notes are there?
+    print(note_count)
+
 noire_img_path = 'solfege/note/noire.png'
 noire_contour, noire_img = create_contour(noire_img_path)
+bad_apple_sheet_path = 'musicsheet/bad_apple_musicsheet.png'
+approximate(noire_img_path, bad_apple_sheet_path)
+
 
 # cv2.approxPloyDP() function to approximate the shape
-approx = cv2.approxPolyDP(
-    noire_contour, 0.01 * cv2.arcLength(noire_contour, True), True)
+# approx = cv2.approxPolyDP(
+#     noire_contour, 0.01 * cv2.arcLength(noire_contour, True), True)
 
 # using drawContours() function
 cv2.drawContours(noire_img, [noire_contour], 0, (0, 0, 255), 1)
 
-cv2.imshow('Contour', noire_img)
+# cv2.imshow('Contour', noire_img)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
