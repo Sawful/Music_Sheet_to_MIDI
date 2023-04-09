75b5b1b52732999b98625fea57fa67011d2dd92e
Colyn Co <colynco2@gmail.com>
2023-04-09 18:00:10 +0200

    Extract horizontal and vertical lines

--- MSM_Main.py
+++ MSM_Main.py
@@ -6,6 +6,7 @@
 
 from PIL import Image
 import cv2
+
 import numpy as np
 
 
@@ -27,53 +28,76 @@ def create_contour(img_path):
     return contours_image[1], image
 
 
-def approximate(reference_image_path, approximated_image_path):
-    # initialise a note counter
-    note_count = 0
+def match(template_image_path, music_sheet_path):
+
     # Initialise both reference image (with its contour) and approximated image
-    reference_image_contour, reference_image = create_contour(reference_image_path)
-    approximated_image = create_contour(approximated_image_path)[1]
-    # Get image dimensions for both images
-    reference_dimensions = reference_image.shape
-    image_dimensions = approximated_image.shape
-    # Approximate contour for the reference image
-    approx_ref = cv2.approxPolyDP(
-        reference_image_contour, 0.01 * cv2.arcLength(reference_image_contour, True), True)
-    # Scans whole image
-    for y in range(image_dimensions[0] - reference_dimensions[0]):
-        for x in range(image_dimensions[1] - reference_dimensions[1]):
-            # Define cropped image
-            cropped_image = approximated_image[x:(x+reference_dimensions[1]), y:(y+reference_dimensions[0])]
-            # Create a contour for each cropped image
-            gray = cv2.cvtColor(cropped_image, cv2.COLOR_BGR2GRAY)
-            _, threshold = cv2.threshold(gray, 200, 255, cv2.THRESH_BINARY)
-            contours_cropped, _ = cv2.findContours(
-                threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-            approx = cv2.approxPolyDP(contours_cropped[0], 0.01 * cv2.arcLength(contours_cropped[0], True), True)
-            # Detect contour and compare if same as reference
-            if len(approx) == len(approx_ref):
-                print("noire")
-                note_count += 1
-    # How many notes are there?
-    print(note_count)
+    template_image_contour, template_image = create_contour(template_image_path)
+    music_sheet = cv2.imread(music_sheet_path)
+
+    # Apply grayscale
+    template_image = cv2.cvtColor(template_image, cv2.COLOR_BGR2GRAY)
+    music_sheet = cv2.cvtColor(music_sheet, cv2.COLOR_BGR2GRAY)
+
+    # Create threshold for both template and viewed image
+    # template_image = cv2.adaptiveThreshold(~template_image, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 15, -2);
+    # music_sheet = cv2.adaptiveThreshold(~music_sheet, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 15, -2);
+
+    _, template_image = cv2.threshold(~template_image, 75, 255, 0)
+    _, music_sheet = cv2.threshold(~music_sheet, 75, 255, 0)
+
+    # ///
+
+    # Create the images that will use to extract the horizontal and vertical lines
+    horizontal = np.copy(music_sheet)
+    vertical = np.copy(music_sheet)
+
+    (rows, cols) = horizontal.shape
+
+    horizontalSize = cols / 30
+    verticalSize = rows / 30
+
+    # Create structure element for extracting horizontal lines through morphology operations
+    horizontalStructure = cv2.getStructuringElement(cv2.MORPH_RECT, (int(horizontalSize), 1))
+
+    cv2.erode(horizontal, horizontalStructure, horizontal)
+
+    cv2.dilate(horizontal, horizontalStructure, horizontal)
+
+    verticalStructure = cv2.getStructuringElement(cv2.MORPH_RECT, (1, int(verticalSize)))
+
+    cv2.erode(vertical, verticalStructure, vertical)
+    cv2.dilate(vertical, verticalStructure, vertical)
+
+    test = cv2.subtract(music_sheet, horizontal)
+
+    cv2.imshow("vertical", test)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    # ///
+
+    # corr = cv2.matchTemplate(music_sheet, template_image,
+    #                          cv2.TM_CCOEFF_NORMED)
+    #
+    # # Find the maximum correlation value and its location
+    # min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(corr)
+    #
+    # # Calculate the top-left and bottom-right corners of the bounding box
+    # top_left = max_loc
+    # bottom_right = (top_left[0] + template_image.shape[1], top_left[1] + template_image.shape[0])
+    #
+    # # Draw a rectangle around the matched region
+    # cv2.rectangle(image, top_left, bottom_right, 0, 2)
+    #
+    # cv2.imshow("Approximate Contour", image)
+    #
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+
+
+bad_apple_sheet_path = 'musicsheet/bad_apple_musicsheet.png'
+bad_apple_image = cv2.imread(bad_apple_sheet_path)
 
 noire_img_path = 'solfege/note/noire.png'
 noire_contour, noire_img = create_contour(noire_img_path)
-bad_apple_sheet_path = 'musicsheet/bad_apple_musicsheet.png'
-approximate(noire_img_path, bad_apple_sheet_path)
 
-
-# cv2.approxPloyDP() function to approximate the shape
-# approx = cv2.approxPolyDP(
-#     noire_contour, 0.01 * cv2.arcLength(noire_contour, True), True)
-
-# using drawContours() function
-cv2.drawContours(noire_img, [noire_contour], 0, (0, 0, 255), 1)
-
-# cv2.imshow('Contour', noire_img)
-
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-# music_sheet_img = Image.open('musicsheet/bad_apple_musicsheet.png')
-# music_sheet_img.show()
+match(noire_img_path, bad_apple_sheet_path)
