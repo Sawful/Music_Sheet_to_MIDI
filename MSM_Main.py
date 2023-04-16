0ec8a1123ab826c9352f65bcdabf5b5fdbb65ea9
Colyn Co <colynco2@gmail.com>
2023-04-16 19:03:09 +0200

    Fill in notes and detect them

--- MSM_Main.py
+++ MSM_Main.py
@@ -10,6 +10,12 @@ import cv2
 import numpy as np
 
 
+def display_image(window_name, img):
+    cv2.imshow(window_name, img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
 def create_contour(img_path):
 
     # reading image
@@ -28,6 +34,47 @@ def create_contour(img_path):
     return contours_image[1], image
 
 
+def remove_lines(thresh_img):
+
+    """
+    thresh_img: threshold image in which you want to remove the lines.
+    """
+    # Create the images that will use to extract the horizontal and vertical lines
+    horizontal = np.copy(thresh_img)
+    vertical = np.copy(thresh_img)
+
+    (rows, cols) = horizontal.shape
+
+    horizontalSize = cols // 30
+    verticalSize = rows // 30
+
+    # Create structure element for extracting horizontal lines through morphology operations
+    horizontalStructure = cv2.getStructuringElement(cv2.MORPH_RECT, (int(horizontalSize), 1))
+
+    cv2.erode(horizontal, horizontalStructure, horizontal)
+
+    cv2.dilate(horizontal, horizontalStructure, horizontal)
+
+    # Create structure element for extracting vertical lines through morphology operations
+    verticalStructure = cv2.getStructuringElement(cv2.MORPH_RECT, (1, int(verticalSize)))
+
+    cv2.erode(vertical, verticalStructure, vertical)
+    cv2.dilate(vertical, verticalStructure, vertical)
+
+    # Remove horizontal
+    horizontal_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (25, 1))
+    detected_lines = cv2.morphologyEx(thresh_img, cv2.MORPH_OPEN, horizontal_kernel, iterations=2)
+    cnts = cv2.findContours(detected_lines, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cnts[0] if len(cnts) <= 2 else cnts[1]
+    for c in cnts:
+        cv2.drawContours(thresh_img, [c], -1, 255, 1)
+
+    repair_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, 3))
+    thresh_img = 255 - cv2.morphologyEx(255 - thresh_img, cv2.MORPH_CLOSE, repair_kernel, iterations=1)
+
+    return thresh_img
+
+
 def match(template_image_path, music_sheet_path):
 
     # Initialise both reference image (with its contour) and approximated image
@@ -38,60 +85,27 @@ def match(template_image_path, music_sheet_path):
     template_image = cv2.cvtColor(template_image, cv2.COLOR_BGR2GRAY)
     music_sheet = cv2.cvtColor(music_sheet, cv2.COLOR_BGR2GRAY)
 
-    # Create threshold for both template and viewed image
-    # template_image = cv2.adaptiveThreshold(~template_image, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 15, -2);
-    # music_sheet = cv2.adaptiveThreshold(~music_sheet, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 15, -2);
-
     _, template_image = cv2.threshold(~template_image, 75, 255, 0)
     _, music_sheet = cv2.threshold(~music_sheet, 75, 255, 0)
 
-    # ///
+    no_line_sheet = remove_lines(music_sheet)
 
-    # Create the images that will use to extract the horizontal and vertical lines
-    horizontal = np.copy(music_sheet)
-    vertical = np.copy(music_sheet)
+    corr = cv2.matchTemplate(no_line_sheet, template_image,
+                             cv2.TM_CCOEFF_NORMED)
 
-    (rows, cols) = horizontal.shape
+    print(corr[322][403])
 
-    horizontalSize = cols / 30
-    verticalSize = rows / 30
+    # Specify a threshold
+    threshold = 0.63
 
-    # Create structure element for extracting horizontal lines through morphology operations
-    horizontalStructure = cv2.getStructuringElement(cv2.MORPH_RECT, (int(horizontalSize), 1))
+    # Store the coordinates of matched area in a numpy array
+    loc = np.where(corr >= threshold)
+    print(loc)
+    # Draw a rectangle around the matched region.
+    for pt in zip(*loc[::-1]):
+        cv2.rectangle(no_line_sheet, pt, (pt[0] + template_image.shape[1], pt[1] + template_image.shape[0]), 255, 1)
 
-    cv2.erode(horizontal, horizontalStructure, horizontal)
-
-    cv2.dilate(horizontal, horizontalStructure, horizontal)
-
-    verticalStructure = cv2.getStructuringElement(cv2.MORPH_RECT, (1, int(verticalSize)))
-
-    cv2.erode(vertical, verticalStructure, vertical)
-    cv2.dilate(vertical, verticalStructure, vertical)
-
-    test = cv2.subtract(music_sheet, horizontal)
-
-    cv2.imshow("vertical", test)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    # ///
-
-    # corr = cv2.matchTemplate(music_sheet, template_image,
-    #                          cv2.TM_CCOEFF_NORMED)
-    #
-    # # Find the maximum correlation value and its location
-    # min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(corr)
-    #
-    # # Calculate the top-left and bottom-right corners of the bounding box
-    # top_left = max_loc
-    # bottom_right = (top_left[0] + template_image.shape[1], top_left[1] + template_image.shape[0])
-    #
-    # # Draw a rectangle around the matched region
-    # cv2.rectangle(image, top_left, bottom_right, 0, 2)
-    #
-    # cv2.imshow("Approximate Contour", image)
-    #
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
+    display_image("no line sheet", no_line_sheet)
 
 
 bad_apple_sheet_path = 'musicsheet/bad_apple_musicsheet.png'
